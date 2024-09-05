42a9885252c2e593022fb8160066ba40ef2588a2
vatsav27 <143064078+vatsav27@users.noreply.github.com>
2024-09-05 15:24:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
-# main.tf
-
 provider "aws" {
   region = "us-east-1"
 }
 
+# VPC
 resource "aws_vpc" "medusa_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Subnet
 resource "aws_subnet" "medusa_subnet" {
   vpc_id            = aws_vpc.medusa_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
+# Security Group
 resource "aws_security_group" "medusa_sg" {
   vpc_id = aws_vpc.medusa_vpc.id
 
@@ -32,12 +34,19 @@ resource "aws_security_group" "medusa_sg" {
   }
 }
 
+# ECS Cluster
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa-cluster"
 }
 
+# ECS Task Definition
 resource "aws_ecs_task_definition" "medusa_task" {
   family                = "medusa-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode           = "awsvpc"
+  cpu                    = "256"
+  memory                 = "512"
+
   container_definitions = jsonencode([{
     name      = "medusa"
     image     = "medusajs/medusa"
@@ -51,18 +60,28 @@ resource "aws_ecs_task_definition" "medusa_task" {
       }
     ]
   }])
+
+  tags = {
+    Name = "medusa-task"
+  }
 }
 
+# ECS Service
 resource "aws_ecs_service" "medusa_service" {
   name            = "medusa-service"
   cluster         = aws_ecs_cluster.medusa_cluster.id
   task_definition = aws_ecs_task_definition.medusa_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+  
   network_configuration {
     subnets          = [aws_subnet.medusa_subnet.id]
     security_groups  = [aws_security_group.medusa_sg.id]
     assign_public_ip = true
+  }
+
+  tags = {
+    Name = "medusa-service"
   }
 }
 
